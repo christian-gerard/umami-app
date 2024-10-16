93488a3a622ab2bf58dc1b305465e1a4c2fa89c3
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-10-16 11:32:11 -0600

    Updating Tailwind CSS Package

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      colors: {
+        'shittake': '#71373B',
+        'white': '#ffffff',
+        'black': '#000000',
+        'champagne': ' #F7E7CE',
+        'gray': '#D3D3D3'
+      },
+      extend: {},
+    },
+    plugins: [],
+  }
