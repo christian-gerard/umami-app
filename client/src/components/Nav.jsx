dd0d0dd79cf4cd9c9f77daeae29d824c44ce0282
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-10-16 15:21:15 -0600

    Adds Mobile Menu

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import { NavLink, useNavigate } from 'react-router-dom'
 import { useContext, useState } from 'react'
 import { UserContext } from '../context/UserContext'
+import CloseIcon from '@mui/icons-material/Close';
 
 function Nav () {
     const {user, logout} = useContext(UserContext)
@@ -26,11 +27,18 @@ function Nav () {
                 </div>
                 {viewMenu && (
 
-                    <div className='sm:hidden flex flex-col bg-shittake text-white z-10 gap-4 italic tracking-widest pl-6 pb-2 rounded-b-lg'>
-                            <NavLink id='link' to='/cookbook' className='hover:text-champagne' > Cookbook </NavLink>
-                            <NavLink id='link' to='/findrecipes' className='hover:text-champagne' > AI Recipes </NavLink>
-                            <NavLink id='link' to='/profile' className='hover:text-champagne'  > Profile </NavLink>
-                            <NavLink id='link' to='/' className='hover:text-champagne' onClick={logout} > Logout </NavLink>
+                    <div className='fixed inset-0 transition-colors backdrop-blur flex justify-center items-center'>
+                        <div className='w-[70%] h-[30%] sm:hidden flex flex-col bg-shittake text-white z-10 gap-4 italic tracking-widest pl-6 pb-2 rounded-lg '>
+                                <div className='h-[20%] flex items-end justify-start'>
+                                    <CloseIcon  onClick={handleMenu} style={{width: '40px', height: '40px'}}/>
+                                </div>
+                                <div className='h-[80%] pl-2 flex flex-col gap-4 text-2xl'>
+                                    <NavLink id='link' to='/cookbook' className='hover:text-champagne' > Cookbook </NavLink>
+                                    <NavLink id='link' to='/findrecipes' className='hover:text-champagne' > AI Recipes </NavLink>
+                                    <NavLink id='link' to='/profile' className='hover:text-champagne'  > Profile </NavLink>
+                                    <NavLink id='link' to='/' className='hover:text-champagne' onClick={logout} > Logout </NavLink>
+                                </div>
+                        </div>
                     </div>
 
                 )}
