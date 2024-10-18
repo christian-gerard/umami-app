8bfd5c2ae4824b01be8530ac18c63f3091cced17
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-10-17 21:43:13 -0600

    Updating Profile Page

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -10,6 +10,12 @@ import { object, string } from 'yup'
 function Profile() {
     const { user, removeUserData, updateUser } = useContext(UserContext)
 
+	const deleteUser = () => {
+		fetch('/api/v1/users', {
+			method: 'DELETE'
+		})
+	}
+
 	console.log(user)
 
     return (
@@ -17,18 +23,20 @@ function Profile() {
 		<div className='w-full h-[92%] flex justify-center items-center'>
 			<div className='bg-champagne size-[70%] rounded-xl text-black text-base'>
 
-				<div className='h-[10%] text-2xl flex items-end px-4 justify-between'>
+				<div className='h-[10%] text-2xl sm:text-2xl flex items-end px-4 justify-between'>
 					<p>Username: { user ? user.username : 'No Name'}</p>
-					<p>Email: {user ? user.email : 'Email Not Listed'}</p>
 				</div>
 
-				<div className='h-[80%] bg-gray px-4'>
+				<div className='h-[80%] bg-gray px-4 text-lg p-2'>
 
+					<p>Email: {user ? user.email : 'Email Not Listed'}</p>
+					<p>Created at: {user.created_at ? user.created_at.slice(0,10) : 'Email Not Listed'}</p>
 					<p>CookBook: {user.recipes.length !== 0 ? user.recipes.length : '0'} Recipes</p>
 
 				</div>
 
-				<div className='h-[10%]'>
+				<div className='h-[10%] flex justify-center items-cente px-4 py-2'>
+					<button className=' w-full bg-shittake text-white rounded-xl p-1'>Delete User</button>
 
 				</div>
 
